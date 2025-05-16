af9481056578a1fd380aae1dd5f995ca369f6943
gabriel-saraiva-flooxai <gabriel.saraiva.flooxai@gmail.com>
2025-05-16 17:56:34 -0300

    feat: use children to compose multiple family groups with members

--- src/index.js
+++ src/index.js
@@ -4,10 +4,19 @@ import ReactDOM from 'react-dom';
 import { Comp1, Comp2 as B } from './components/DoisComponentes';
 import MultiElementos from './components/MultiElementos';
 import FamiliaSilva from './components/FamiliaSilva';
+import Familia from './components/Familia';
+import Membro from './components/Membro';
 
 const elemento = document.getElementById('root')
 ReactDOM.render(
   <div >
-    <FamiliaSilva />
+    <Familia>
+      <Membro nome='Daniel' sobrenome='Pereira' />
+      <Membro nome='Mariana' sobrenome='Pereira' />
+    </Familia>
+    <Familia>
+      <Membro nome='Era só mais um' sobrenome='Silva' />
+      <Membro nome='Q a estrela ñ' sobrenome='Brilha' />
+    </Familia>
   </div>
 , elemento)
