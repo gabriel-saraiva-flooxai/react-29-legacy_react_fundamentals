4379e1136714842ba62246d3b1cb555e7a1607c2
gabriel-saraiva-flooxai <gabriel.saraiva.flooxai@gmail.com>
2025-05-19 14:47:33 -0300

    docs: demonstrate stateless behavior in functional component

--- src/components/Pai.jsx
+++ src/components/Pai.jsx
@@ -4,10 +4,15 @@ import Filho from "./Filho";
 
 export default props => {
 
-    const notificarSaidaDoFilho = lugar => alert(`Liberado para ${lugar}`)
+    let lugarInformado = "Nenhum"
+    const notificarSaidaDoFilho = lugar => {
+        lugarInformado = lugar //alert(`Liberado para ${lugar}`)
+        console.log(lugar);
+    }
 
     return (
         <div>
+            {lugarInformado}
             <Filho notificarSaida={notificarSaidaDoFilho} />
         </div>
     )
