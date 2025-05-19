69a8366b7becf63c789a064a653349d301404346
gabriel-saraiva-flooxai <gabriel.saraiva.flooxai@gmail.com>
2025-05-19 14:30:25 -0300

    feat: implement parent-child callback communication

--- src/index.js
+++ src/index.js
@@ -7,10 +7,12 @@ import FamiliaSilva from './components/FamiliaSilva';
 import Familia from './components/Familia';
 import Membro from './components/Membro';
 import ComponenteComFuncao from './components/ComponenteComFuncao';
+import Pai from './components/Pai';
 
 const elemento = document.getElementById('root')
 ReactDOM.render(
   <div >
+    <Pai />
     <Familia sobrenome='dos Santos' cep='Santos'>
       <Membro nome='Fabiana' />
       <Membro nome='Mariana' />
